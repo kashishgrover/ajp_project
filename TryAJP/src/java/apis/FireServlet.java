be2aad1d128691840590ed3bc2f27bf8da5b50ba
Hriddhi Dey <hriddhidey@gmail.com>
2016-11-10 17:27:56 +0530

    Base servlet code

---
+++ TryAJP/src/java/apis/FireServlet.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apis;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author HD
+ */
+public class FireServlet extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FireServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet FireServlet at " + request.getContextPath() + "</h1>");
+            out.println("<a href='portal.html'>Back</a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+}
